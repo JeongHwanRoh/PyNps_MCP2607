4a3610d7d9f1ab936c70ee844dd395962be36b70
노정환 <108844707+JeongHwanRoh@users.noreply.github.com>
2026-08-25 20:02:55 +0900

    Add title and company name input to Streamlit app

--- streamlit_main.py
+++ streamlit_main.py
@@ -118,7 +118,15 @@ def read_pensiondata():
 
 data=read_pensiondata()
 
-company_name=st.text_input("회사명을 입력해 주세요", placeholder="검색할 회사명 입력")
+
 
 st.title("국민연금 데이터 분석")
+
+company_name=st.text_input("회사명을 입력해 주세요", placeholder="검색할 회사명 입력")
+
+if data and company_name:
+    output=data.find_company(company_name=company_name)
+    if len(output)>0:
+        st.subheader(output.iloc[0]['사업장명'])
+
 
